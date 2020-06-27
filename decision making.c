2a3f4243d090e2e7b892fede039c38cf9395ed4b
shloka08 <67062858+shloka08@users.noreply.github.com>
2020-06-27 11:35:16 +0530

    created decision making.c
    task to replicate the program and to make as many innovative changes to it as possible.

---
+++ decision making.c
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int age;
+  cout<<"Enter Age of user:";
+  cin>>age;
+  if(age>=18)
+  {
+    cout<<"\lnYou are eligible";
+  }
+  else
+    cout<<"\ln You are not eligible";
+    return 0;
+  }  
